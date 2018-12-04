1eb03db2ab209e3e2bbfbae3e92f9a8df2a650f8
LamNguyenViet <34784624+LamNguyenViet@users.noreply.github.com>
2018-12-04 23:47:59 +0700

    java
    intern

---
+++ InternJava/src/main/java/com/tamdongtam/java/InternJavaApplication.java
@@ -0,0 +1,12 @@
+package com.tamdongtam.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternJavaApplication.class, args);
+	}
+}
